efaefee9f10f8f60b2a42125a324f15d884589c2
ArnoldHenry <arnoldomwoyo2@gmail.com>
2018-05-16 12:36:06 +0300

    added js code for video effect

---
+++ js/php.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+});
+
+var plynow = document.getElementById('btn');
+var darktheme = document.getElementById("dark");
+
+plynow.onclick = function(){
+  darktheme.style.display = "block";
+}
